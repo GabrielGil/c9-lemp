0645df2cae146b8b394786e52b155b09bbfa7b1b
Gabriel Gil <gabriel@gabrielgil.es>
2015-01-29 07:55:30 +0000

    Updated and cleaned up the script

--- install.sh
+++ install.sh
@@ -1,45 +1,50 @@
 #!/bin/bash
-#set -e
+
+
 
 # Redirect stdout ( > ) into a named pipe ( >() ) running "tee"
 exec > >(tee /tmp/installlog.txt)
-
-# Without this, only stdout would be captured - i.e. your
-# log file would not contain any error messages.
+# Without this, only stdout would be captured
 exec 2>&1
 
-#
+
+
 # Stop all the services
-#
 
 # Apache2
 sudo service apache2 stop
-
 # NGINX
 sudo service nginx stop
-
 # MySQL
 mysql-ctl stop
 
-# Set Up PHP
-sudo sed -i 's/user = www-data/user = ubuntu/g' /etc/php5/fpm/pool.d/www.conf
-sudo sed -i 's/group = www-data/group = ubuntu/g' /etc/php5/fpm/pool.d/www.conf
 
-# Set up MySQL
-mysql-ctl install
 
-# Set up NGINX
+# Set them up!
+
+
+# NGINX:
 # Listen port 80, change document root, setup indexes, configure PHP sock
 # set up the try_url thing (Drupal is not Worpress)...
 # Thankfully, I already modified this in the repo!
 sudo wget https://raw.githubusercontent.com/GabrielGil/c9-lemp/master/default --output-document=/etc/nginx/sites-available/default
+
+# PHP:
+sudo sed -i 's/user = www-data/user = ubuntu/g' /etc/php5/fpm/pool.d/www.conf
+sudo sed -i 's/group = www-data/group = ubuntu/g' /etc/php5/fpm/pool.d/www.conf
+
+# MySQL:
+mysql-ctl install
+
+
 
 # Start the party!
 sudo service nginx start
 sudo service php5-fpm start
 mysql-ctl start
 
+
+
 # Are we ready?
 sudo service nginx status
 sudo service php5-fpm status
-mysql-ctl status
