d276966bd7f8cb36e8b4a2c3f75f0e76356f09b6
Sebastián Contreras <sebascontre@users.noreply.github.com>
2016-01-02 12:07:33 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-
-
 # Redirect stdout ( > ) into a named pipe ( >() ) running "tee"
 exec > >(tee /tmp/installlog.txt)
 # Without this, only stdout would be captured
 exec 2>&1
-
-
 
 # Stop all the services
 
@@ -18,10 +14,12 @@ sudo service nginx stop
 # MySQL
 mysql-ctl stop
 
-
+# Instaling PHP7.0
+sudo add-apt-repository ppa:ondrej/php-7.0
+sudo apt-get update
+sudo apt-get install php7.0 php7.0-fpm
 
 # Set them up!
-
 
 # NGINX:
 # Listen port 80, change document root, setup indexes, configure PHP sock
@@ -37,26 +35,18 @@ sudo sed -i 's/user = www-data/user = ubuntu/g' /etc/php5/fpm/pool.d/www.conf
 sudo sed -i 's/group = www-data/group = ubuntu/g' /etc/php5/fpm/pool.d/www.conf
 sudo sed -i 's/pm = dynamic/pm = ondemand/g' /etc/php5/fpm/pool.d/www.conf # Reduce number of processes..
 
-# MySQL:
-mysql-ctl install
-
-
 # Install helper
 sudo wget https://raw.githubusercontent.com/GabrielGil/c9-lemp/master/lemp --output-document=/usr/bin/lemp
 sudo chmod 755 /usr/bin/lemp
 
 
 # Start the party!
-mysql-ctl start
 sudo service nginx start
 sudo service nginx reload
-sudo service php5-fpm start
-
-
+sudo service php7.0-fpm start
 
 # Are we ready?
 echo Check all services are up.
-sleep 5 # Wait for MySQL server to be fully loaded.
+sleep 5
 sudo service nginx status
 sudo service php5-fpm status
-mysql-ctl status
